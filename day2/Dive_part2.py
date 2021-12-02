6b2c46b18c91362ec9e9d67269b919930b7067d7
Lukas Schiermeier <11963410+lschiermeier@users.noreply.github.com>
2021-12-02 21:25:45 +0100

    slightly improved day 2

--- day2/Dive_part2.py
+++ day2/Dive_part2.py
@@ -4,19 +4,18 @@ depth = 0
 lines = None
 with open('day2/input.txt', 'r') as fp:
     lines = fp.readlines()
+
 commands = [x.strip().split(" ") for x in lines]
+commands = [(x, int(y)) for x, y in commands]
 
 for c in commands:
     if c[0] == "forward":
-        hor_pos += int(c[1])
-        depth += int(c[1]) * aim
+        hor_pos += c[1]
+        depth += c[1] * aim
     elif c[0] == "down":
-        aim += int(c[1])
+        aim += c[1]
     else:
-        aim -= int(c[1])
-
-
-
+        aim -= c[1]
 
 print(f"Current Aim: {aim}")
 print(f"Depth Pos: {depth}")
