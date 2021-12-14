88f7683160a147ad3c77e261aae5b7505df6de76
Lukas Schiermeier <11963410+lschiermeier@users.noreply.github.com>
2021-12-14 13:42:02 +0100

    day 14 done

--- day14/Extended_Polymerization.py
+++ day14/Extended_Polymerization.py
@@ -27,3 +27,44 @@ print(f"Result Part 1: {mc_num-lc_num}")
 print(f"{mc_element} occurred: {mc_num} times")
 print(f"{lc_element} occurred: {lc_num} times")
 
+# good
+expansions = {k: k[0]+v+k[1] for k,v in rules.items()}
+additions = {k:[v[0:2],v[1:3]] for k,v in expansions.items()}
+occurrencesTemplate = [template.count(e) for e in elements]
+
+ruleCountsTemplate = {}
+for i in range(len(template)-1):
+    k = template[i:i+2]
+    ruleCountsTemplate[k] = ruleCountsTemplate.get(k,0) + 1
+
+def doBetterStep(ruleCounts):
+    newRuleCounts = {}
+    for k, v in ruleCounts.items():
+        a, b = additions[k]
+        newRuleCounts[a] = newRuleCounts.get(a,0) + v
+        newRuleCounts[b] = newRuleCounts.get(b,0) + v
+    return newRuleCounts
+
+def convertRuleCounts(ruleCounts):
+    elementCounts = {e:0 for e in elements}
+    for k,v in ruleCounts.items():
+        elementCounts[k[0]] += v
+    elementCounts[template[-1]] +=1
+    return elementCounts
+
+ruleCountsPart2 = ruleCountsTemplate.copy()
+for i in range(40):
+    ruleCountsPart2 = doBetterStep(ruleCountsPart2)
+
+eCounts = convertRuleCounts(ruleCountsPart2)
+
+c2e = {v:k for k,v in eCounts.items()}
+mc_num = max(c2e.keys())
+lc_num = min(c2e.keys())
+mc_element = c2e[mc_num]
+lc_element = c2e[lc_num]
+
+print(f"Result Part 2: {mc_num-lc_num}")
+print(f"{mc_element} occurred: {mc_num} times")
+print(f"{lc_element} occurred: {lc_num} times")
+
