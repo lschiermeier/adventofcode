b17a5f047c364e114eabf9aa1c0696bb04a737b1
Lukas Schiermeier <11963410+lschiermeier@users.noreply.github.com>
2021-12-17 15:09:37 +0100

    started catchup by finishing day 10, no recursion though

--- day10/Syntax_Scoring.py
+++ day10/Syntax_Scoring.py
@@ -1,32 +1,60 @@
 #!python3.10
 
-# with open('day10/input.txt', 'r') as fp:
-with open('day10/testinput.txt', 'r') as fp:
+from types import LambdaType
+from statistics import median
+
+with open('day10/input.txt', 'r') as fp:
+# with open('day10/testinput.txt', 'r') as fp:
     lines = [x.strip() for x in fp.readlines()]
 
 char_pairs = {"(":")","[":"]","{":"}","<":">"}
+openers = list(char_pairs.keys())
+closers = list(char_pairs.values())
 
-illegal_points = {")":3, "]":57, "}":1197, ">": 25137}
-
-
-def corruption_rec(c, cs):
-    match cs:
-        case [closing, next, *cs_cont] if closing == char_pairs[c]:
-            print(c,closing, next, cs_cont)
-            return corruption_rec(next, cs_cont)
-        case [closing, *cs_cont] if closing in char_pairs.values():
-            return False, closing
-        case [opening ,*cs_cont] if opening in char_pairs.keys():
-            functional, cs_rest = corruption_rec(opening, cs_cont)
-        case _:
-            pass
+corruptor_score = {")":3, "]":57, "}":1197, ">": 25137}
 
 
 
-def is_corrupted(line):
-    c, cs = line[0], line[1:]
-    while len(cs) > 0 and functional:
-        functional, cs = corruption_rec(c,cs)
-    return functional, cs
+def test_corruption(line) -> tuple[bool, object]:
+    cs = list(line)
+    stack = []
+    for c in cs:
+        if c in openers:
+            stack.append(c)
+        else:
+            if len(stack) == 0:
+                return True, c
+            opener = stack.pop()
+            if c != char_pairs[opener]:
+                return True, c
+    return False, stack
 
-corruption_rec("(",list(")[]()()"))
+
+
+
+incomplete = []
+stacks = []
+corruptors = []
+for l in lines:
+    corrupted, obj = test_corruption(l)
+    if not corrupted:
+        incomplete.append(l)
+        stacks.append(obj)
+    else:
+        corruptors.append(obj)
+
+error_score = sum(map(lambda x: corruptor_score[x],corruptors))
+
+print(f"Result Part 1: {error_score}")
+
+completer_score = {")":1, "]":2, "}":3, ">":4}
+
+scores = []
+for l, s in zip(incomplete, stacks):
+    scores.append(sum([completer_score[char_pairs[c]]*5**i for i,c in enumerate(s)]))
+completion_score = median(scores)
+
+print(f"Result Part 2: {completion_score}")
+
+
+pass
