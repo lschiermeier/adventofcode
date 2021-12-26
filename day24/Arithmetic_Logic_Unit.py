a2af56da249aecfd30ed1c7ff1d09e5217eba5cb
Lukas Schiermeier <11963410+lschiermeier@users.noreply.github.com>
2021-12-26 01:29:17 +0100

    day 24 done

--- day24/Arithmetic_Logic_Unit.py
+++ day24/Arithmetic_Logic_Unit.py
@@ -22,35 +22,42 @@ def runMONADp(inp, commands, state):
 
 inp_idxs = [idx for idx, l in enumerate(lines) if l[0] == "inp"] + [len(lines)]
 
-start_state = {reg: 0 for reg in regs}
-monads = [""]
-possible_states = [{} for _ in range(15)]
-possible_states[0][""] = start_state
-found_best = False
-counter = 0
-while not found_best:
-    counter += 1
-    if not counter % 1000:
-        print(counter, len(monads),monads[-1])
-    monad = monads.pop()
-    d = len(monad)
-    state = possible_states[d][monad]
-    new_monads = []
-    cmds = lines[inp_idxs[d]:inp_idxs[d+1]]
-    for x in range(9,0,-1):
-        new_m = monad + str(x)
-        new_state = runMONADp(x, cmds, state.copy())
-        if  new_state["z"] < 1000000:
-            if not new_state in possible_states[d+1].values() and len(new_m) < 14:
-                possible_states[d+1][new_m] = new_state
-                new_monads.insert(0,new_m)
-            elif len(new_m) == 14 and new_state["z"] == 0:
-                found_best = True
-                winning_monad = new_m
-                winning_state = new_state
-                break
-    monads = monads + new_monads
+def findValid(backward):
+    start_state = {reg: 0 for reg in regs}
+    monads = [""]
+    possible_states = [{} for _ in range(15)]
+    possible_states[0][""] = start_state
+    found_best = False
+    counter = 0
+    while not found_best:
+        counter += 1
+        if not counter % 1000:
+            print(counter, len(monads),monads[-1])
+        monad = monads.pop()
+        d = len(monad)
+        state = possible_states[d][monad]
+        new_monads = []
+        cmds = lines[inp_idxs[d]:inp_idxs[d+1]]
+        for x in (range(9,0,-1) if backward else range(1,10)):
+            new_m = monad + str(x)
+            new_state = runMONADp(x, cmds, state.copy())
+            if  new_state["z"] < 1000000:
+                if not new_state in possible_states[d+1].values() and len(new_m) < 14:
+                    possible_states[d+1][new_m] = new_state
+                    new_monads.insert(0,new_m)
+                elif len(new_m) == 14 and new_state["z"] == 0:
+                    found_best = True
+                    winning_monad = new_m
+                    winning_state = new_state
+                    return winning_monad, winning_state
+        monads = monads + new_monads
 
+winning_monad, winning_state = findValid(True)
 print(f"Result Part 1: {winning_monad}")
 print(f"End State = {winning_state}")
+
+winning_monad, winning_state = findValid(False)
+print(f"Result Part 2: {winning_monad}")
+print(f"End State = {winning_state}")
+
 pass
