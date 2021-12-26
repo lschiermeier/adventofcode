3e32890af0e991bc49de368ec3c6795e956a6339
Lukas Schiermeier <11963410+lschiermeier@users.noreply.github.com>
2021-12-26 12:59:54 +0100

    finished day 12

--- day12/Passage_Pathing.py
+++ day12/Passage_Pathing.py
@@ -1,11 +1,14 @@
+
 with open('day12/input.txt', 'r') as fp:
 # with open('day12/testinput.txt', 'r') as fp:
+# with open('day12/smallinput.txt', 'r') as fp:
     lines = [x.strip().split("-") for x in fp.readlines()]
 
 class Node:
     def __init__(self,name:str) -> None:
         self.name = name
         self.isBig = name.isupper()
+        self.isTerminal = name in ["end", "start"]
         self.edges = []
 
     def add_edge(self, other:'Node'):
@@ -13,22 +16,37 @@ class Node:
         other.edges.append(self)
 
 class Path:
-    def __init__(self,start:Node):
+    def __init__(self,start:Node,usedJoker):
         self.start = start
         self.nodes = [start]
+        self.usedJoker = usedJoker
     def add_node(self, next:Node) -> int:
+        """ returns true on success """
         if next.isBig:
             self.nodes.append(next)
-            return 1
+            return True
         elif next in self.nodes:
-            return 0
+            if not self.usedJoker and not next.isTerminal:
+                self.nodes.append(next)
+                self.usedJoker = True
+                return True
+            else:
+                return False
         else:
             self.nodes.append(next)
             return True
     def copy(self):
-        new = Path(self.start)
+        new = Path(self.start, self.usedJoker)
         new.nodes = self.nodes.copy()
         return new
+    def is_ended(self):
+        return self.nodes[-1].name == "end"
+
+def expand(path:Path):
+    next_Nodes = path.nodes[-1].edges
+    paths = [path.copy() for _ in next_Nodes]
+    ext_path_sel = list(map(Path.add_node, paths, next_Nodes))
+    return [p for p,sel in zip(paths,ext_path_sel) if sel]
 
 web = {}
 for a, b in lines:
@@ -38,14 +56,24 @@ for a, b in lines:
         web[b] = Node(b)
     web[a].add_edge(web[b])
 
-def expand(path:Path):
-    next_Nodes = path.nodes[-1].edges
-    paths = [path.copy() for e in next_Nodes]
-    extended_ps = list(map(Path.add_node, paths, next_Nodes))
-    return paths[extended_ps]
+finished_paths = []
+working_paths = [Path(web["start"], True)]
+while working_paths:
+    print(len(working_paths),len(finished_paths))
+    new_paths = [new_p for old_p in working_paths for new_p in expand(old_p)]
+    ended_sel = list(map(Path.is_ended, new_paths))
+    finished_paths += [p for p,sel in zip(new_paths, ended_sel) if sel]
+    working_paths = [p for p,sel in zip(new_paths, ended_sel) if not sel]
 
-all_paths = []
-done = False
-while not done:
-    
-    pass
+print(f"Result Part 1: {len(finished_paths)}")
+
+finished_paths = []
+working_paths = [Path(web["start"], False)]
+while working_paths:
+    print(len(working_paths),len(finished_paths))
+    new_paths = [new_p for old_p in working_paths for new_p in expand(old_p)]
+    ended_sel = list(map(Path.is_ended, new_paths))
+    finished_paths += [p for p,sel in zip(new_paths, ended_sel) if sel]
+    working_paths = [p for p,sel in zip(new_paths, ended_sel) if not sel]
+
+print(f"Result Part 2: {len(finished_paths)}")
