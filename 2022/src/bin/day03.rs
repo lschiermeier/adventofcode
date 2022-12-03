29b0bdf6ae812a076f8fd1392f5805c8648322ce
Lukas Schiermeier <11963410+lschiermeier@users.noreply.github.com>
2022-12-03 18:36:43 +0100

    Day 3 reformated

--- 2022/src/bin/day03.rs
+++ 2022/src/bin/day03.rs
@@ -24,23 +24,33 @@ fn main() {
     let mut all_doubles = Vec::<char>::new();
     for (first_comp, second_comp) in &rucksacks {
         let doubles: Vec<&char> = first_comp.intersection(&second_comp).collect();
-        assert!(doubles.len() == 1, "Line didn't contain exactly one double...");
+        assert!(
+            doubles.len() == 1,
+            "Line didn't contain exactly one double..."
+        );
         all_doubles.push(doubles[0].to_owned());
     }
 
     let result: u32 = all_doubles.iter().map(letter_to_score).sum();
     println!("Result Day 3 Part 1: {}", result);
 
-
     let mut badges = Vec::<char>::new();
-    for  team  in rucksacks.chunks(3) {
-        let elf_1:HashSet<_> = team[0].0.union(&team[0].1).cloned().collect();
-        let elf_2:HashSet<_> = team[1].0.union(&team[1].1).cloned().collect();
-        let elf_3:HashSet<_> = team[2].0.union(&team[2].1).cloned().collect();
-        let team_intersection: HashSet<char> = elf_1.intersection(&elf_2).cloned().collect();
-        let team_intersection: HashSet<char> = team_intersection.intersection(&elf_3).cloned().collect();
+    for team in rucksacks.chunks(3) {
+        let team: Vec<_> = team
+            .iter()
+            .map(|x| x.0.union(&x.1).cloned().collect::<HashSet<char>>())
+            .collect();
+        let team_intersection: HashSet<char> = team[0].intersection(&team[1]).cloned().collect();
+        let team_intersection: HashSet<char> =
+            team_intersection.intersection(&team[2]).cloned().collect();
         assert!(team_intersection.len() == 1, "wrong badge found");
-        badges.push(team_intersection.iter().next().expect("intersection went wrong").to_owned())
+        badges.push(
+            team_intersection
+                .iter()
+                .next()
+                .expect("intersection went wrong")
+                .to_owned(),
+        )
     }
     let result: u32 = badges.iter().map(letter_to_score).sum();
     println!("Result Day 3 Part 2: {}", result);
@@ -50,6 +60,10 @@ fn letter_to_score(letter: &char) -> u32 {
     if char::is_lowercase(*letter) {
         letter.to_digit(36).expect("wasn't lower case") - 9
     } else {
-        letter.to_ascii_lowercase().to_digit(36).expect("conversion didn't work") + 17
+        letter
+            .to_ascii_lowercase()
+            .to_digit(36)
+            .expect("conversion didn't work")
+            + 17
     }
 }
