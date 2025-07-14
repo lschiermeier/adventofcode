ccb7ef32ab8694e559f640a7a30b0b103469f9ec
Lukas Schiermeier <11963410+lschiermeier@users.noreply.github.com>
2025-07-14 15:33:39 +0200

    Continued Day 2.

--- 2024/src/bin/day02.rs
+++ 2024/src/bin/day02.rs
@@ -1,4 +1,5 @@
 use aoc_2024::*;
+// use log;
 
 fn main() {
     let src_name = file!();
@@ -7,11 +8,28 @@ fn main() {
 
     let num_table: Vec<Vec<i64>> = parse_table(table);
     let sum_p1 = num_table.iter().filter(|x| determine_safety(x)).count();
+    let sum_p2 = num_table.iter().filter(|x| determine_dampened_safety(x)).count();
 
     println!("{src_name} - Result 1: {sum_p1}");
-    // println!("{src_name} - Result 2: {}");
+    println!("{src_name} - Result 2: {sum_p2}");
 }
 
 fn determine_safety(report: &Vec<i64>) -> bool {
-    todo!();
+    if report.is_sorted_by(|a,b| a < b && a + 3 >= *b) {
+        // println!("{report:?}");
+        // println!("report is sorted growing.");
+        true
+    } else if report.is_sorted_by(|a,b| a > b && *a <= b + 3) {
+        // println!("{report:?}");
+        // println!("report is sorted shrinking.");
+        true
+    } else {
+        // println!("{report:?}");
+        // println!("report is not or incorrectly sorted.");
+        false
+    }
+}
+
+fn determine_dampened_safety(report: &Vec<i64>) -> bool {
+    todo!()
 }
